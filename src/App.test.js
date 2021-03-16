a1395bcbb5c18c49bf3538ee51dcc2aabc73279a
Michael Brooks <mike.brooks.23@hotmail.co.uk>
2021-03-16 14:46:48 +0000

    added docker file and tests passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function sum(a, b) {
+  return a + b;
+}
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(sum(1, 2)).toBe(3);
 });
